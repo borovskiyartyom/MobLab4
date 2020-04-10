08351f3dfa26ec023786ef9f287e60aabacfed5e
Artyom <51659803+borovskiyartyom@users.noreply.github.com>
2020-04-10 23:20:48 +0300

    This is 1 of 3 part of 4 lab
    This is 1 of 3 part of 4 lab

---
+++ lr_4_1/app/src/main/java/com/example/lr_4_1/ControlActivity2.java
@@ -0,0 +1,20 @@
+package com.example.lr_4_1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ControlActivity2 extends Fragment {
+
+    //Для работы с фрагментами
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_2, container, false);
+    }
+}
